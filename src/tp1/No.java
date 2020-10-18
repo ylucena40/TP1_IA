232ffcfb6dadabc34b001353196c8920143cbf42
Yan Lucena <47032404+ylucena40@users.noreply.github.com>
2020-10-18 20:48:57 -0300

    Create No.java

---
+++ src/tp1/No.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yanlu
+ */
+public class No {
+    private int missionario;
+    private int canibal;
+    private int side;
+    private int deep;
+    private ArrayList<No> filhos;
+
+    public int getMissionario() {
+        return missionario;
+    }
+
+    public void setMissionario(int missionario) {
+        this.missionario = missionario;
+    }
+
+    public int getCanibal() {
+        return canibal;
+    }
+
+    public void setCanibal(int canibal) {
+        this.canibal = canibal;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getDeep() {
+        return deep;
+    }
+
+    public void setDeep(int deep) {
+        this.deep = deep;
+    }
+
+    public ArrayList<No> getFilhos() {
+        return filhos;
+    }
+    
+    public void setHead(int mis, int can, int side, int deep){
+        this.missionario = mis;
+        this.canibal = can;
+        this.side = side;
+        this.deep = deep;
+        this.filhos = new ArrayList<No>();
+    }
+    
+}
