6cdb0bf689b8ba6636af97212db04cd6ce29f671
Yan Lucena <47032404+ylucena40@users.noreply.github.com>
2020-10-20 15:01:39 -0300

    Update Tp1.java

--- src/tp1/Tp1.java
+++ src/tp1/Tp1.java
@@ -13,15 +13,21 @@ public class Tp1 {
         // TODO code application logic here
 
         head.setHead(3, 3, 1, 0);
+        //setar a cabeça da arvoré,sendo (N Missionarios, N Canibais, Lado do Rio (1 esquerdo 0 direito), profundidade da árvore)
         search();
     }
     
     public static void search(){
         No prev = new No();
         prev.setHead(-1, -1, -1, -1);
+        //setar um nó antecessor com valores que não são válidos
+        //para na hora de observar o nó anterior do nó atual
+        //para adicionar um nó na árvore que não seja repetido
+        //não tenha problema com a cabeça da árvore
         for(int i = 0; i < profMax; i++){
             System.out.println("Prof: " + i);
             System.out.println("---------------");
+            //fazer a parte interativa da busca, limitando o aprofundamento que a busca irá fazer
             createGraph(head, prev,i);
             System.out.println("---------------");
         }
@@ -31,20 +37,31 @@ public class Tp1 {
         System.out.println(recent.getMissionario() + "  " + recent.getCanibal() + "  " + recent.getSide() + "  " + recent.getDeep());
         if(prof == recent.getDeep()){
             return;
+            //caso eu chegue no limite de profundidade passada para a função, eu retorno ele para que eu não vá além desse limite
         }
         if (recent.getMissionario() == 0 && recent.getCanibal() == 0 && recent.getSide() == 0) {
             return;
+            //caso eu chegue no meu nó objetivo que é (0,0,0) eu retorno a função e não vejo se terá nós filhos
         }
         No newNo = new No();
+        //regras de validações para passar do lado esquerdo do rio para o lado direito 
         if (recent.getSide() == 1) {
-            //diminuir
-
+            //
+            //como o código só analisa o lado esquerdo do rio, sempre que irei passar para o lado direito o barco
+            //eu sempre retiro do meu valor atual a quantidade de missionarios e canibais que tenho
+            //primeiro caso passar para o lado direito 1 missionario
             if (recent.getMissionario() > 0) {
                 newNo.setMissionario(recent.getMissionario() - 1);
                 newNo.setCanibal(recent.getCanibal());
                 newNo.setSide(0);
                 newNo.setDeep(recent.getDeep() + 1);
                 newNo.setFilhos();
+                //como olho apenas o lado esquerdo, caso o valor total de missionarios menos o valor de missionarios que eu passei
+                //seja menor que o valor total de canibais menos o valor de atuais de canibais do lado esquerdo do rio
+                //significa que eu tenho uma posição inválida, por ter mais canibais do que missionarios do lado direito do rio
+                //sendo 3 - nMissionario >= 3 - nCanibal
+                //sendo assim, o resultado da primeira conta tem q ser sempre maior igual
+                //e verifico tbm, se o valor atual de missionarios é maior que o de canibais do lado esquerdo
                 if (newNo.getMissionario() < 3 && (3 - newNo.getMissionario() >= (3 - newNo.getCanibal())) && newNo.getMissionario() >= newNo.getCanibal()) {
                     validateNo(head, newNo);
                     if (createNo) {
@@ -54,7 +71,7 @@ public class Tp1 {
                     createNo = true;
                 }
             }
-            //
+            ////segundo caso passar para o lado direito 1 canibal
             if (recent.getCanibal() > 0) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario());
@@ -62,6 +79,11 @@ public class Tp1 {
                 newNo.setSide(0);
                 newNo.setDeep(recent.getDeep() + 1);
                 newNo.setFilhos();
+                //refaço a conta do valor total de missionarios menos o valor atual de missionarios para ver se é maior que o valor total
+                //de canibais menos o valor atual para verificar o lado direito do rio para que eu possa passar 1 canibal para lá
+                //sendo 3 - nMissionario <= 3 - nCanibal
+                //é o inverso da de cima pois eu posso ter o caso de não ter passado nenhum missinário para o lado direito do rio
+                //já o else if a conta de verificação é igual a anterior, pois tenho missionários do lado direito do rio
                 if (newNo.getMissionario() == 3 && (3 - newNo.getMissionario() <= (3 - newNo.getCanibal()))) {
                     validateNo(head, newNo);
                     if (createNo) {
@@ -78,7 +100,7 @@ public class Tp1 {
                     createNo = true;
                 }
             }
-            //
+            //terceiro caso passar para o lado direito 2 missionarios
             if (recent.getMissionario() > 1) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario() - 2);
@@ -106,7 +128,7 @@ public class Tp1 {
                     }
                 }
             }
-            //
+            //quarto caso passar para o lado direito 2 canibais
             if (recent.getCanibal() > 1) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario());
@@ -130,7 +152,7 @@ public class Tp1 {
                     createNo = true;
                 }
             }
-            //
+            //quinto caso passar para o lado direito 1 canibal e 1 missionario
             if (recent.getMissionario() > 0 && recent.getCanibal() > 0) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario() - 1);
@@ -150,7 +172,8 @@ public class Tp1 {
             return;
 
         } else {
-            //
+            //regras para passar do lado direito do rio para o lado esquerdo]
+            //primeiro caso passar para o lado esquerdo 1 missionario
             if (recent.getMissionario() < 3) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario() + 1);
@@ -158,6 +181,8 @@ public class Tp1 {
                 newNo.setSide(1);
                 newNo.setDeep(recent.getDeep() + 1);
                 newNo.setFilhos();
+                //utilizo a formula novamente com o >= para verificar se o número de missionarios no lado direito do rio é maior ou igual
+                //e depois verifico se do lado esquerdo do rio é maior ou igual também
                 if ((3 - newNo.getMissionario() >= (3 - newNo.getCanibal()))) {
                     if (newNo.getMissionario() >= newNo.getCanibal()) {
                         validateNo(head, newNo);
@@ -170,7 +195,7 @@ public class Tp1 {
                 }
             }
 
-            //
+            //segundo caso passar para o lado esquerdo 1 canibal
             if (recent.getCanibal() < 3) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario());
@@ -195,7 +220,7 @@ public class Tp1 {
                 }
             }
 
-            //
+            //segundo caso passar para o lado esquerdo 2 missionarios
             if (recent.getMissionario() < 2) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario() + 2);
@@ -215,7 +240,7 @@ public class Tp1 {
                 }
             }
 
-            //
+            //segundo caso passar para o lado esquerdo 2 canibais
             if (recent.getCanibal() < 2) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario());
@@ -240,7 +265,7 @@ public class Tp1 {
                     createNo = true;
                 }
             }
-            //
+            //segundo caso passar para o lado esquerdo 1 canibal e 1 missionario
             if (recent.getMissionario() < 3 && recent.getCanibal() < 3) {
                 newNo = new No();
                 newNo.setMissionario(recent.getMissionario() + 1);
@@ -262,13 +287,16 @@ public class Tp1 {
             return;
         }
     }
-
+    //função para verificar se um Nó já existe na árvore
     public static void validateNo(No recent, No newNo) {
+        //caso o nó que estou verificando seja o meu nó objetivo, eu já retorno a função direto para que ele possa ser adicionado
         if (newNo.getMissionario() == 0
                 && newNo.getCanibal() == 0
                 && newNo.getSide() == 0) {
             return;
         }
+        //caso exista o nó, eu verifico se a profundidade do nó que eu quero adicionar é maior que o nó que já existe na árovre
+        //caso ele seja maior eu não adiciono por ser o caso de um nó antecessor do nó q eu quero adicionar
         if (newNo.getMissionario() == recent.getMissionario()
                 && newNo.getCanibal() == recent.getCanibal()
                 && newNo.getSide() == recent.getSide()) {
@@ -277,6 +305,8 @@ public class Tp1 {
             }
             return;
         }
+        //se o meu nó recente tiver filhos, eu percorro todos para verificar se algum dele é igual ao nó que quero adicionar
+        //utilizando de recursão para poder percorrer toda a árvore
         if (recent.getFilhos() != null) {
             for (int i = 0; i < recent.getFilhos().size(); i++) {
                 validateNo(recent.getFilhos().get(i), newNo);
